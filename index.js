41336db8bc43fecfdee07dcb9982d1e3351b64c6
saurabh-23b0131030 <rishi678p@gmail.com>
2025-04-08 10:36:23 +0530

    lol

---
+++ index.js
@@ -0,0 +1,41 @@
+// let varcontainingFunction = function () {
+//     let varInFunction = "I'm in a function";
+//     console.log("hi there!", varInFunction);
+// }
+// varcontainingFunction();
+// function parameter(args1, args2) {
+//     return args1 + " " + args2;
+// }
+// const args1 = "hello";
+// const args2 = "javascript";
+// console.log(parameter(args1, args2));
+
+// function addTwoNumbers(x, y) {
+//     console.log(x + y);
+// }
+// const x = 10;
+// const y = 20;
+// addTwoNumbers(x, y);
+
+// function addTwoNumbers(){
+//     let ip=prompt("enter the number")
+//     let ou=prompt("enter the number")
+//     return parseInt(ip)+parseInt(ou)
+// }
+// console.log(addTwoNumbers());
+// function addTwoNumbers(x = 3, y = 7) {
+//     console.log(x + y);
+// }
+// addTwoNumbers(5, 6, 8, 3);
+
+// hello = () => {
+//     return "Hello World!";
+//   } 
+//   console.log(hello());
+
+// let addTwoNumbers = (x, y) => console.log(x+y);
+// addTwoNumbers(5, 2);
+
+// const arr = ["apple", "banana", "cherry"];
+// const arr2 = ["elephant", ...arr, "monkey"]
+// console.log(arr2);
